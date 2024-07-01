50ac1c14270b398c7c888404f092e9275fdcf63f
Jazier Corbera T. <jazier@outlook.com>
2024-07-01 13:58:08 -0500

    Added Upload File to Container with Service Principal

--- 03_UploadFiletoContainerWithServicePrincipal/main.py
+++ 03_UploadFiletoContainerWithServicePrincipal/main.py
@@ -1,18 +1,22 @@
-import os
-from dotenv import load_dotenv
-from azure.storage.blob import BlobServiceClient
+from azure.identity import ClientSecretCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
+# Información del Service Principal
+sp_tenant_id = "6d94013f-6c4c-484e-bbd7-2b95b7fe0f87"
+sp_client_id = "82ba1b93-1278-41c2-b054-bbd6896538ea"
+sp_client_secret_value = "bec8Q~OPIPNoN3wNFbRy9lhPoGLCIklGrS9RTblx"
 
-load_dotenv()
-
-# Configuración
-connection_string = os.environ["CONNECTION_STRING"]
+# Información de la cuenta de almacenamiento
+storage_account_url = "https://storageaccountjazier.blob.core.windows.net/"
 container_name = "containerforlearning"
-local_file_path = "C:/Users/Jazier/Desktop/Azure/01_UploadFiletoContainerWithKey/mytext.txt"
-blob_name = "mytextcopied.txt"
+blob_name = "example03.txt"  # Nombre del archivo en el contenedor
+local_file_path = "C:/Users/Administrator/Desktop/azure/03_UploadFiletoContainerWithServicePrincipal/mytext.txt"  # Ruta al archivo local
+
+# Autenticación
+credential = ClientSecretCredential(tenant_id=sp_tenant_id, client_id=sp_client_id, client_secret=sp_client_secret_value)
 
 # Crear el cliente de BlobService
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+blob_service_client = BlobServiceClient(storage_account_url, credential=credential)
 
 # Crear el cliente del contenedor
 container_client = blob_service_client.get_container_client(container_name)
@@ -24,4 +28,4 @@ blob_client = container_client.get_blob_client(blob_name)
 with open(local_file_path, "rb") as data:
     blob_client.upload_blob(data, overwrite=True)
 
-print(f"Archivo {local_file_path} subido a {blob_name} en el contenedor {container_name}.")
+print(f"Archivo subido a {blob_name} en el contenedor {container_name}.")
